48f1c7201cb807a28f7e138dc576f8c58242a908
Shota Iguchi <shota-iguchi@cookpad.com>
2023-06-12 15:19:01 +0900

    Initial Commit

---
+++ authapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  authapp
+//
+//  Created by shota-iguchi on 2023/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
